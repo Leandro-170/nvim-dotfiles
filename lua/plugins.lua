41987e85e9b842d837ecdb10d68801beeada2f76
Leandro-170 <73649765+Leandro-170@users.noreply.github.com>
2023-05-01 18:40:57 -0300

    ensure packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,24 @@
-require('packer').init({
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+local packer = require 'packer'
+
+packer.init({
   display = {
     open_fn = require('packer.util').float
   }
 })
 
-require('packer').startup(function(use)
+packer.startup(function(use)
 
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig'
@@ -40,6 +54,10 @@ require('packer').startup(function(use)
   }
   use 'nvim-lua/plenary.nvim'
   use 'Civitasv/cmake-tools.nvim'
+
+  if packer_bootstrap then
+    packer.sync()
+  end
 
 end)
 
